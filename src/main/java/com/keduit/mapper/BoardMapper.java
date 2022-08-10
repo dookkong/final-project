6eaf1e697e358e7f412b30b4aa0e55f9a1c2dc71
prani <102644423+JINSEOP15@users.noreply.github.com>
2022-08-10 17:33:52 +0900

    08.10

--- src/main/java/com/keduit/mapper/BoardMapper.java
+++ src/main/java/com/keduit/mapper/BoardMapper.java
@@ -3,13 +3,18 @@ package com.keduit.mapper;
 import java.util.List;
 
 import com.keduit.domain.BoardVO;
+import com.keduit.domain.Criteria;
 
 public interface BoardMapper {
 	//list 뿌려주기
 	public List<BoardVO> getlist();
 	
+	public List<BoardVO> getListWithPaging(Criteria cri);
+	
 	//게시글 insert
 	public void insert(BoardVO vo);
+	
+	public void insertSelectKey(BoardVO board);
 	
 	//게시글 조회 (키 값(bno)를 이용하여 read)
 	public BoardVO read(Long bno); 
@@ -19,4 +24,6 @@ public interface BoardMapper {
 	
 	//게시글 수정
 	public int update(BoardVO vo);
+	
+	public int getTotalCount(Criteria cri);
 }
