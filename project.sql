85bbfaf09d08b6e8d6835d7616a7e7104cfd881e
bitna1218 <93469531+bitna1218@users.noreply.github.com>
2022-08-17 23:45:15 +0900

    0817 sql 업뎃

--- project.sql
+++ project.sql
@@ -1,4 +1,4 @@
--- 1. »∏ªÁ∆Ú∞° ======================================================================================
+-- 1. ÌöåÏÇ¨ÌèâÍ∞Ä ======================================================================================
 CREATE SEQUENCE seq_CompanyVal;
 
 CREATE  TABLE CompanyVal_TB(
@@ -11,13 +11,15 @@ updatedate DATE DEFAULT sysdate,
 views NUMBER(10,0) DEFAULT 0,
 likes NUMBER(10,0) DEFAULT 0,
 CONSTRAINT CompanyVal_bno PRIMARY KEY (bno),
-CONSTRAINT CompanyVal_userid foreign key(userid) references Member_TB(userid)
+CONSTRAINT CompanyVal_userid foreign key(userid) references Member_TB(userid) ON DELETE CASCADE
 );
+
+ALTER TABLE CompanyVal_TB add replyCnt NUMBER(10,0) DEFAULT 0;
 
 select * from CompanyVal_TB; 
 drop TABLE CompanyVal_TB;
 
--- 2. ∏È¡¢¡˙πÆ∞¯¿Ø ======================================================================================
+-- 2. Î©¥Ï†ëÏßàÎ¨∏Í≥µÏú† ======================================================================================
 
 CREATE SEQUENCE seq_InterViewAns;
 
@@ -31,12 +33,14 @@ updatedate DATE DEFAULT sysdate,
 views NUMBER(10,0) DEFAULT 0,
 likes NUMBER(10,0) DEFAULT 0,
 CONSTRAINT InterviewAns_bno PRIMARY KEY (bno),
-CONSTRAINT InterviewAns_userid foreign key(userid) references Member_TB(userid)
+CONSTRAINT InterviewAns_userid foreign key(userid) references Member_TB(userid) ON DELETE CASCADE
 );
+ 
+ ALTER TABLE InterviewAns_TB add replyCnt NUMBER(10,0) DEFAULT 0;
  
  select * from InterViewAns_TB;
  drop TABLE InterviewAns_TB;
--- 3. «’∞›»ƒ±‚ ======================================================================================
+-- 3. Ìï©Í≤©ÌõÑÍ∏∞ ======================================================================================
 CREATE SEQUENCE seq_Passlatter;
   
 CREATE  TABLE Passlatter_TB(
@@ -49,13 +53,15 @@ updatedate DATE DEFAULT sysdate,
 views NUMBER(10,0) DEFAULT 0,
 likes NUMBER(10,0) DEFAULT 0,
 CONSTRAINT Passlatter_bno PRIMARY KEY (bno),
-CONSTRAINT Passlatter_userid foreign key(userid) references Member_TB(userid)
+CONSTRAINT Passlatter_userid foreign key(userid) references Member_TB(userid) ON DELETE CASCADE
 );
 
+ ALTER TABLE Passlatter_TB add replyCnt NUMBER(10,0) DEFAULT 0;
+ 
  select * from Passlatter_TB;
 drop TABLE Passlatter_TB;
   
--- 4. ∞ÌπŒQ&A ======================================================================================
+-- 4. Í≥†ÎØºQ&A ======================================================================================
  
 CREATE SEQUENCE seq_WorryQnA;
   
@@ -70,13 +76,14 @@ views NUMBER(10,0) DEFAULT 0,
 likes NUMBER(10,0) DEFAULT 0,
 category VARCHAR2(50) not null,
 CONSTRAINT WorryQnA_bno PRIMARY KEY (bno),
-CONSTRAINT WorryQnA_userid foreign key(userid) references Member_TB(userid)
+CONSTRAINT WorryQnA_userid foreign key(userid) references Member_TB(userid) ON DELETE CASCADE
 );
 
+ ALTER TABLE WorryQnA_TB add replyCnt NUMBER(10,0) DEFAULT 0;
  select * from WorryQnA_TB;
  drop TABLE WorryQnA_TB;
  
--- 5. «’∞›¿⁄º“º≠∞¯¿Ø======================================================================================
+-- 5. Ìï©Í≤©ÏûêÏÜåÏÑúÍ≥µÏú†======================================================================================
 CREATE SEQUENCE seq_PassSelfIntroduct;
   
 CREATE  TABLE PassSelfIntroduct_TB(
@@ -89,13 +96,14 @@ updatedate DATE DEFAULT sysdate,
 views NUMBER(10,0) DEFAULT 0,
 likes NUMBER(10,0) DEFAULT 0,
 CONSTRAINT PassSelfIntroduct_bno PRIMARY KEY (bno),
-CONSTRAINT PassSelfIntroduct_userid foreign key(userid) references Member_TB(userid)
+CONSTRAINT PassSelfIntroduct_userid foreign key(userid) references Member_TB(userid) ON DELETE CASCADE
 );
 
+ ALTER TABLE PassSelfIntroduct_TB add replyCnt NUMBER(10,0) DEFAULT 0;
  select * from PassSelfIntroduct_TB;
  drop TABLE PassSelfIntroduct_TB;
 
--- 6. πÆ¿«∞‘Ω√∆«======================================================================================
+-- 6. Î¨∏ÏùòÍ≤åÏãúÌåê======================================================================================
 
 CREATE SEQUENCE seq_Ask;
   
@@ -108,35 +116,52 @@ regdate DATE DEFAULT sysdate,
 updatedate DATE DEFAULT sysdate,
 category VARCHAR2(50) not null,
 CONSTRAINT Ask_bno PRIMARY KEY (bno),
-CONSTRAINT Ask_userid foreign key(userid) references Member_TB(userid)
+CONSTRAINT Ask_userid foreign key(userid) references Member_TB(userid) ON DELETE CASCADE
 );
 
- select * from Ask_TB;
+ALTER TABLE Ask_TB add replyCnt NUMBER(10,0) DEFAULT 0;
+select * from Ask_TB;
 drop TABLE Ask_TB;
 
- -- 7. »∏ø¯ ======================================================================================
-
-CREATE SEQUENCE seq_Member;
-
- CREATE  TABLE Member_TB(
- bno NUMBER(10,0),
- username VARCHAR2(50) not null,
- password VARCHAR2(50) not null,
- userid VARCHAR2(50) not null,
- userphone VARCHAR2(50) not null,
- regdate DATE DEFAULT sysdate,
- updatedate DATE DEFAULT sysdate,
- auth VARCHAR2(50) DEFAULT 'ROLE_USER',
- CONSTRAINT Member_userid PRIMARY KEY (userid)
- ); 
+ -- 7. ÌöåÏõê ======================================================================================
  
-select * from Member_TB; 
+create table Member_TB(
+userid VARCHAR2(50) not null primary key,
+userpw VARCHAR2(100) not null,
+username VARCHAR2(100) not null,
+userphone VARCHAR2(50) not null,
+regdate date default sysdate,
+updatedate date default sysdate,
+enabled char(1) default '1');
 
+select * from Member_TB; 
 drop TABLE Member_TB;
+
+select * from Member_TB order by regdate desc;
+
+delete from Member_TB where userid= 'ÏïÑÏù¥Îîî3';
 
 commit;
 
- -- 8. »∏ªÁ∆Ú∞° ¥Ò±€DB ======================================================================================
+ -- 8. ÌöåÏõêÍ∂åÌïú ======================================================================================
+ 
+CREATE  TABLE Member_Auth_TB(
+userid VARCHAR2(50) not null,
+auth VARCHAR2(50) DEFAULT 'ROLE_USER',
+constraint Member_Auth_fk foreign key(userid)references Member_TB(userid) ON DELETE CASCADE
+ );
+
+select * from Member_Auth_TB; 
+
+drop TABLE Member_Auth_TB;
+
+insert into Member_Auth_TB(userid) select userid from Member_TB;
+
+ALTER TABLE Member_Auth_TB ADD CONSTRAINT Member_Auth_fk FOREIGN KEY (userid) REFERENCES Member_TB(userid) ON DELETE CASCADE;
+
+update Member_Auth_TB set auth='ROLE_ADMIN' where userid='user1';
+
+ -- 9. ÌöåÏÇ¨ÌèâÍ∞Ä ÎåìÍ∏ÄDB ======================================================================================
 CREATE SEQUENCE seq_CompanyVal_reply;
  
 CREATE TABLE CompanyVal_reply_TB(
@@ -147,13 +172,15 @@ CREATE TABLE CompanyVal_reply_TB(
  regdate DATE DEFAULT sysdate,
  updatedate DATE DEFAULT sysdate,
  CONSTRAINT CompanyVal_reply_rno PRIMARY KEY (rno),
- CONSTRAINT CompanyVal_reply_bno foreign key(bno) references CompanyVal_TB(bno),
- CONSTRAINT CompanyVal_reply_userid foreign key(userid) references Member_TB(userid)
+ CONSTRAINT CompanyVal_reply_bno foreign key(bno) references CompanyVal_TB(bno)ON DELETE CASCADE,
+ CONSTRAINT CompanyVal_reply_userid foreign key(userid) references Member_TB(userid) ON DELETE CASCADE
  ); 
  
  select * from CompanyVal_reply_TB; 
+ drop TABLE CompanyVal_reply_TB;
  
- -- 9. ∏È¡¢¡˙πÆ∞¯¿Ø¥Ò±€DB ======================================================================================
+ 
+ -- 10. Î©¥Ï†ëÏßàÎ¨∏Í≥µÏú†ÎåìÍ∏ÄDB ======================================================================================
  CREATE SEQUENCE seq_InterviewAns_reply;
  
 CREATE TABLE InterviewAns_reply_TB(
@@ -164,13 +191,14 @@ CREATE TABLE InterviewAns_reply_TB(
  regdate DATE DEFAULT sysdate,
  updatedate DATE DEFAULT sysdate,
  CONSTRAINT InterviewAns_reply_rno PRIMARY KEY (rno),
- CONSTRAINT InterviewAns_reply_bno foreign key(bno) references InterviewAns_TB(bno),
- CONSTRAINT InterviewAns_reply_userid foreign key(userid) references Member_TB(userid)
+ CONSTRAINT InterviewAns_reply_bno foreign key(bno) references InterviewAns_TB(bno)ON DELETE CASCADE,
+ CONSTRAINT InterviewAns_reply_userid foreign key(userid) references Member_TB(userid) ON DELETE CASCADE
  ); 
  
- select * from InterviewAns_reply_TB; 
+select * from InterviewAns_reply_TB; 
+drop TABLE InterviewAns_reply_TB;
  
- -- 10. «’∞›»ƒ±‚ ¥Ò±€DB ======================================================================================
+ -- 11. Ìï©Í≤©ÌõÑÍ∏∞ ÎåìÍ∏ÄDB ======================================================================================
 CREATE SEQUENCE seq_Passlatter_reply;
  
 CREATE TABLE Passlatter_reply_TB(
@@ -181,12 +209,14 @@ CREATE TABLE Passlatter_reply_TB(
  regdate DATE DEFAULT sysdate,
  updatedate DA;TE DEFAULT sysdate,
  CONSTRAINT Passlatter_reply_rno PRIMARY KEY (rno),
- CONSTRAINT Passlatter_reply_bno foreign key(bno) references Passlatter_TB(bno),
- CONSTRAINT Passlatter_reply_userid foreign key(userid) references Member_TB(userid)
+ CONSTRAINT Passlatter_reply_bno foreign key(bno) references Passlatter_TB(bno)ON DELETE CASCADE,
+ CONSTRAINT Passlatter_reply_userid foreign key(userid) references Member_TB(userid) ON DELETE CASCADE
  ); 
  
  select * from Passlatter_reply_TB; 
- -- 11. ∞ÌπŒQ&A ¥Ò±€DB ======================================================================================
+ drop TABLE Passlatter_reply_TB;
+ 
+ -- 11. Í≥†ÎØºQ&A ÎåìÍ∏ÄDB ======================================================================================
  CREATE SEQUENCE seq_WorryQnA_reply;
  
 CREATE TABLE WorryQnA_reply_TB(
@@ -197,13 +227,14 @@ CREATE TABLE WorryQnA_reply_TB(
  regdate DATE DEFAULT sysdate,
  updatedate DATE DEFAULT sysdate,
  CONSTRAINT WorryQnA_reply_rno PRIMARY KEY (rno),
- CONSTRAINT WorryQnA_reply_bno foreign key(bno) references WorryQnA_TB(bno),
- CONSTRAINT WorryQnA_reply_userid foreign key(userid) references Member_TB(userid)
+ CONSTRAINT WorryQnA_reply_bno foreign key(bno) references WorryQnA_TB(bno)ON DELETE CASCADE,
+ CONSTRAINT WorryQnA_reply_userid foreign key(userid) references Member_TB(userid) ON DELETE CASCADE
  ); 
  
- select * from Passlatter_reply_TB; 
+ select * from WorryQnA_reply_TB; 
+ drop TABLE WorryQnA_reply_TB;
  
- -- 12. «’∞›¿⁄º“º≠∞¯¿Ø¥Ò±€DB ======================================================================================
+ -- 13. Ìï©Í≤©ÏûêÏÜåÏÑúÍ≥µÏú†ÎåìÍ∏ÄDB ======================================================================================
 CREATE SEQUENCE seq_PassSelfIntroduct_reply;
  
 CREATE TABLE PassSelfIntroduct_reply_TB(
@@ -214,13 +245,14 @@ CREATE TABLE PassSelfIntroduct_reply_TB(
  regdate DATE DEFAULT sysdate,
  updatedate DATE DEFAULT sysdate,
  CONSTRAINT PassSelfIntroduct_reply_rno PRIMARY KEY (rno),
- CONSTRAINT PassSelfIntroduct_reply_bno foreign key(bno) references PassSelfIntroduct_TB(bno),
- CONSTRAINT PassSelfIntroduct_reply_userid foreign key(userid) references Member_TB(userid)
+ CONSTRAINT PassSelfIntroduct_reply_bno foreign key(bno) references PassSelfIntroduct_TB(bno) ON DELETE CASCADE, 
+ CONSTRAINT PassSelfIntroduct_reply_userid foreign key(userid) references Member_TB(userid) ON DELETE CASCADE
  ); 
  
- select * from Passlatter_reply_TB;
- 
- -- 13. πÆ¿«∞‘Ω√∆«¥Ò±€DB ======================================================================================
+select * from PassSelfIntroduct_reply_TB;
+drop TABLE PassSelfIntroduct_reply_TB;
+  
+ -- 14. Î¨∏ÏùòÍ≤åÏãúÌåêÎåìÍ∏ÄDB ======================================================================================
  
 CREATE SEQUENCE seq_Ask_reply;
  
@@ -232,18 +264,17 @@ CREATE TABLE Ask_reply_TB(
  regdate DATE DEFAULT sysdate,
  updatedate DATE DEFAULT sysdate,
  CONSTRAINT Ask_reply_rno PRIMARY KEY (rno),
- CONSTRAINT Ask_reply_bno foreign key(bno) references Ask_TB(bno),
- CONSTRAINT Ask_reply_userid foreign key(userid) references Member_TB(userid)
+ CONSTRAINT Ask_reply_bno foreign key(bno) references Ask_TB(bno) ON DELETE CASCADE,
+ CONSTRAINT Ask_reply_userid foreign key(userid) references Member_TB(userid) ON DELETE CASCADE
  ); 
  
- select * from Passlatter_reply_TB;
+select * from Ask_reply_TB;
+drop TABLE Ask_reply_TB;
  
  
+ --======================================================================================
  
- 
- 
- 
- 
+
  
  
  
